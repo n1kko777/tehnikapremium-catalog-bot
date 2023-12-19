f4983e85f48558006b76eb33b1b138f04694eb3d
Nikita Tarasov <3734489@gmail.com>
2023-12-19 07:49:15 +0500

    300

--- parser.js
+++ parser.js
@@ -86,7 +86,7 @@ async function updateItems(items) {
         results.push(item);
       }
 
-      await new Promise((r) => setTimeout(r, 100));
+      await new Promise((r) => setTimeout(r, 300));
       index++;
     } catch (error) {
       console.error(error);
